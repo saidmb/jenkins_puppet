05e74857710a78787bed3d6016d0158f3ed689c7
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-04 23:41:31 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -17,25 +17,32 @@ ini_setting { "Jenkins Port Setting":
   value   => '8000',
 }
 
+class jenkins_puppet {
 
-exec{'download':
-  command      =>  'wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo',
-  path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-  cwd          =>  '/etc/yum.repos.d',
-  user         =>  'root',
-  creates      =>  '/etc/yum.repos.d/jenkins.repo',
-  notify       =>  Exec['extract'],
-}
+  ######################### Dowload Jenkins Repo ############################
+  exec{'download':
+    command      =>  'wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo',
+    path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    cwd          =>  '/etc/yum.repos.d',
+    user         =>  'root',
+    creates      =>  '/etc/yum.repos.d/jenkins.repo',
+    notify       =>  Exec['extract'],
+  }
 
-exec{'extract':
-  command      =>  'rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key',
-  path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-  cwd          =>  '/etc/yum.repos.d',
-  user         =>  'root',
-  refreshonly  =>  true,
-}
+  ######################### Extract Jenkins Repo locally ############################
+  exec{'extract':
+    command      =>  'rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key',
+    path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    cwd          =>  '/etc/yum.repos.d',
+    user         =>  'root',
+    refreshonly  =>  true,
+  }
 
-package{'jenkins':
-  ensure       =>  installed,
+  package{'jenkins':
+    ensure       =>  installed,
+  }
+  
+  ############################################
+  
 }
 
