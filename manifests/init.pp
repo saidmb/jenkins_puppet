92dfea04bddc59382131ecffd9df924b27f2ba94
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 00:59:28 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,5 @@
 class jenkins_puppet {
 
-  ######################### Dowload Jenkins Repo ############################
-  exec{'download':
-    command      =>  'wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo',
-    path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    cwd          =>  '/etc/yum.repos.d',
-    user         =>  'root',
-    creates      =>  '/etc/yum.repos.d/jenkins.repo',
-    notify       =>  Exec['extract'],
-  }
-
-  ######################### Extract Jenkins Repo locally ############################
-  exec{'extract':
-    command      =>  'rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key',
-    path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    cwd          =>  '/etc/yum.repos.d',
-    user         =>  'root',
-    refreshonly  =>  true,
-  }
-
-  package{'jenkins':
-    ensure       =>  installed,
-  }
-  
   ############################################
   
 
@@ -45,5 +22,9 @@ ini_setting { "Jenkins Port Setting":
   setting => 'JENKINS_PORT',
   value   => '8000',
 }
+
+
+class {'jenkins': }  
+
 
 }
