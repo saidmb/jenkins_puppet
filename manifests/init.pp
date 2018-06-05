31be9210750b0b2995b03211442ca0277fa6700d
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 00:39:29 -0700

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,3 @@
-/************************
-
 class jenkins_puppet {
 
   ######################### Dowload Jenkins Repo ############################
@@ -27,6 +25,24 @@ class jenkins_puppet {
   
   ############################################
   
+
+node default {
+  file {'/root/README':
+    ensure  => file,
+    content => "This is a sample README 321\n",
+    owner   => 'root',
+  }
 }
 
-************************/
+package {'java':
+  ensure => present,
+}
+
+ini_setting { "Jenkins Port Setting":
+  ensure  => present,
+  path    => '/etc/sysconfig/jenkins',
+  setting => 'JENKINS_PORT',
+  value   => '8000',
+}
+
+}
