815fae8916be8075705e65f5ad4bb4e979ccaf19
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-04 23:34:36 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -16,3 +16,26 @@ ini_setting { "Jenkins Port Setting":
   setting => 'JENKINS_PORT',
   value   => '8000',
 }
+
+
+exec{'download':
+  command      =>  'wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo',
+  path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+  cwd          =>  '/etc/yum.repos.d',
+  user         =>  'root',
+  creates      =>  '/etc/yum.repos.d/jenkins.repo',
+  notify       =>  Exec['extract'],
+}
+
+exec{'extract':
+  command      =>  'rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key',
+  path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+  cwd          =>  '/etc/yum.repos.d',
+  user         =>  'root',
+  refreshonly  =>  true,
+}
+
+package{'jenkins':
+  ensure       =>  installed,
+}
+
