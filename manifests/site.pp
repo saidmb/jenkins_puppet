27b5be0fcef64cdbc9484bcfb7aef793de209961
saidmb <8784942+saidmb@users.noreply.github.com>
2018-06-05 00:33:52 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,31 @@
+class jenkins_puppet {
+
+  ######################### Dowload Jenkins Repo ############################
+  exec{'download':
+    command      =>  'wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo',
+    path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    cwd          =>  '/etc/yum.repos.d',
+    user         =>  'root',
+    creates      =>  '/etc/yum.repos.d/jenkins.repo',
+    notify       =>  Exec['extract'],
+  }
+
+  ######################### Extract Jenkins Repo locally ############################
+  exec{'extract':
+    command      =>  'rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key',
+    path         =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    cwd          =>  '/etc/yum.repos.d',
+    user         =>  'root',
+    refreshonly  =>  true,
+  }
+
+  package{'jenkins':
+    ensure       =>  installed,
+  }
+  
+  ############################################
+  
+
 node default {
   file {'/root/README':
     ensure  => file,
@@ -15,4 +43,6 @@ ini_setting { "Jenkins Port Setting":
   path    => '/etc/sysconfig/jenkins',
   setting => 'JENKINS_PORT',
   value   => '8000',
+}
+
 }
